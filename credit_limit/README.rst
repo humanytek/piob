f4348093795919a663a93728beacb27bf0758d63
Luis Quintero <luis.quintero9419@gmail.com>
2022-10-18 13:12:10 -0500

    [ADD] credit_limit

---
+++ credit_limit/README.rst
@@ -0,0 +1,3 @@
+Credit Limit
+============
+Habilita un nuevo campo `credit_limit` en el modelo `res.partner`
---
+++ credit_limit/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Credit Limit",
+    "version": "15.0.1.0.0",
+    "author": "Humanytek",
+    "depends": [],
+    "data": [
+        "views/res_partner.xml",
+    ],
+}
