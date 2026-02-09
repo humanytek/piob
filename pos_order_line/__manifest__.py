a4942d12ae1d376983d1627f7a0a7447bfe64a46
RamonArcaLabs <ramon.reyna@arcalabs.com.mx>
2026-02-09 14:27:53 -0600

    add pos order line module

---
+++ pos_order_line/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "PoS Order Line Tree View",
+    "version": "19.0.1.2.0",
+    "author": "Humanytek",
+    "license": "LGPL-3",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "security/pos_order_line.xml",
+        "views/pos_order_line.xml",
+    ],
+}
